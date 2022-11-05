481883c7e5c0f91c48a70cce20e7973692e4393a
UngVanHung <113441405+UngVanHung@users.noreply.github.com>
2022-11-05 16:10:15 +0700

    Update control7SEG.c

--- control7SEG.c
+++ control7SEG.c
@@ -122,13 +122,13 @@ void display7SEG()
 			break;
 		}
 	default:
-					HAL_GPIO_WritePin(SEG_A_GPIO_Port, SEG_A_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_B_GPIO_Port, SEG_B_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_C_GPIO_Port, SEG_C_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_D_GPIO_Port, SEG_D_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_E_GPIO_Port, SEG_E_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_F_GPIO_Port, SEG_F_Pin, RESET);
-					HAL_GPIO_WritePin(SEG_G_GPIO_Port, SEG_G_Pin, RESET);
+					HAL_GPIO_WritePin(SEG_A_GPIO_Port, SEG_A_Pin, SET);
+					HAL_GPIO_WritePin(SEG_B_GPIO_Port, SEG_B_Pin, SET);
+					HAL_GPIO_WritePin(SEG_C_GPIO_Port, SEG_C_Pin, SET);
+					HAL_GPIO_WritePin(SEG_D_GPIO_Port, SEG_D_Pin, SET);
+					HAL_GPIO_WritePin(SEG_E_GPIO_Port, SEG_E_Pin, SET);
+					HAL_GPIO_WritePin(SEG_F_GPIO_Port, SEG_F_Pin, SET);
+					HAL_GPIO_WritePin(SEG_G_GPIO_Port, SEG_G_Pin, SET);
 					break;
 }
 }
